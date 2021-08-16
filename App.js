2031559106a0f31f32e21bdb0ea0dcdc3e01aa55
DMoya07 <davidmoya2014@gmail.com>
2021-08-16 16:23:24 -0600

    Ejercicio Clase 3

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, } from 'react-native';
+
+const estilos=StyleSheet.create({
+  pregunta:{
+    color: '#D5EEBB',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: '25px',
+    padding: '10px'
+  },
+
+  respuesta:{
+    color: '#E7E0C9',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: '25px',
+    padding: '10px'
+  }
+})
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={estilos.pregunta}>Nombre</Text><Text style={estilos.respuesta}>David Enrique Moya</Text><br></br>
+      <Text style={estilos.pregunta}>Carnet</Text><Text style={estilos.respuesta}>MA170877</Text><br></br>
+      <Text style={estilos.pregunta}>Pasatiempo</Text><Text style={estilos.respuesta}>Jugar Videojuegos</Text><br></br>
+      <Text style={estilos.pregunta}>Lenguaje de Programación Favorito</Text><Text style={estilos.respuesta}>PHP</Text><br></br>
+      <Text style={estilos.pregunta}>Película Favorita</Text>
       <StatusBar style="auto" />
+      <Image
+      style={{width: 300, height: 300}}
+      
+      source={require('./assets/BTTF.jpg') }
+      />
     </View>
   );
 }
@@ -14,7 +41,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#6B7AA1',
     alignItems: 'center',
     justifyContent: 'center',
   },
